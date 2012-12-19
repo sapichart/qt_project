6d452d884b13c5b2e44077453ed672a907f88a7a
sapichart <sapichart@shipco.com>
2012-12-19 13:05:00 +0700

    HUNG : Try first change

--- src/com/shipco/hackathon/QT_Hackathon.java
+++ src/com/shipco/hackathon/QT_Hackathon.java
@@ -1,26 +1,29 @@
 package com.shipco.hackathon;
 
-import com.trolltech.qt.gui.*;
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QWidget;
 
 public class QT_Hackathon extends QWidget {
 
-    Ui_QT_Hackathon ui = new Ui_QT_Hackathon();
+	Ui_QT_Hackathon ui = new Ui_QT_Hackathon();
 
-    public static void main(String[] args) {
-        QApplication.initialize(args);
+	public static void main(String[] args) {
+		QApplication.initialize(args);
 
-        QT_Hackathon testQT_Hackathon = new QT_Hackathon();
-        testQT_Hackathon.show();
+		QT_Hackathon testQT_Hackathon = new QT_Hackathon();
+		testQT_Hackathon.show();
 
-        QApplication.exec();
-    }
+		// Make change for GIT Try.
 
-    public QT_Hackathon() {
-        ui.setupUi(this);
-    }
+		QApplication.exec();
+	}
 
-    public QT_Hackathon(QWidget parent) {
-        super(parent);
-        ui.setupUi(this);
-    }
+	public QT_Hackathon() {
+		ui.setupUi(this);
+	}
+
+	public QT_Hackathon(QWidget parent) {
+		super(parent);
+		ui.setupUi(this);
+	}
 }
